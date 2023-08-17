c65608e12a32f144307570e702560d1753779630
SouravMane333 <souravmane97@gmail.com>
2023-08-17 14:30:25 +0530

    Initial commit

---
+++ lib/data.dart
@@ -0,0 +1,15 @@
+List<String> images = [
+  "assets/image_04.jpeg",
+  "assets/image_03.jpeg",
+  "assets/image_02.jpeg",
+  "assets/image_01.jpeg",
+  "assets/lock.jpeg",
+];
+
+List<String> title = [
+  "Hounted Ground",
+  "Fallen In Love",
+  "The Dreaming Moon",
+  "Jack the Persian and the Black Castel",
+  "Lock",
+];
